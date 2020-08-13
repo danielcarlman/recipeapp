07b385c838a9291194b184be4582243c1c92b224
danielcarlman <daniel.v.carlman@gmail.com>
2020-08-12 18:54:34 -0600

    Organized CSS Variables alphabetically

--- src/Components/DropDownMenu/index.js
+++ src/Components/DropDownMenu/index.js
@@ -6,19 +6,18 @@ function DropDownMenu() {
   return (
     <select name='units' className='dropdown-item'>
       <option selected>unit</option>
-      <option id='tablespoon'>tbsp</option>
-      <option id='teaspoon'>tsp</option>
-      <option id='cups'>cups</option>
-      <option id='milliliter'>ml</option>
-      <option id='liter'>L</option>
+      <option id='cups'>cup</option>
       <option id='gallon'>gal</option>
-      <option id='pint'>pint</option>
-      <option id='gram'>g</option>
-      <option id='ounce'>oz</option>
-      <option id='pound'>lb</option>
-      <option id='milligram'>mg</option>
       <option id='gram'>g</option>
       <option id='kilogram'>kg</option>
+      <option id='liter'>L</option>
+      <option id='milligram'>mg</option>
+      <option id='milliliter'>ml</option>
+      <option id='ounce'>oz</option>
+      <option id='pint'>pint</option>
+      <option id='pound'>lb</option>
+      <option id='tablespoon'>tbsp</option>
+      <option id='teaspoon'>tsp</option>
     </select>
   );
 }
