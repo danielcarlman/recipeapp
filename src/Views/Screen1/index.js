29557c109e2c3b2f157d31c9327b7b8d0497b74d
danielcarlman <daniel.v.carlman@gmail.com>
2020-09-08 05:14:27 -0600

    Created conditional rendering for title and ingredient screens

--- src/Views/Screen1/index.js
+++ src/Views/Screen1/index.js
@@ -1,55 +1,169 @@
 import React, { useState } from 'react';
-// eslint-disable-next-line no-unused-vars
-import style from './style/style.css';
 import { Actions } from '../../redux/reducers/Recipes';
 import { useSelector, useDispatch } from 'react-redux';
 import Input from '../../Components/Input';
 import Button from '../../Components/Button';
-import { Link } from 'react-router-dom';
+import DropDownMenu from '../../Components/DropDownMenu';
+// import { Link } from 'react-router-dom';
 
 function Screen1() {
-  const { title } = useSelector((state) => state.Recipes);
   const dispatch = useDispatch();
+  const { title, ingredients } = useSelector((state) => state.Recipes);
+  const [ingredientTitle, setIngredientTitle] = useState('');
+  const [screen, changeScreen] = useState(false);
 
   function setTitle(title) {
     dispatch(Actions.setTitle(title));
   }
 
-  return (
-    <div className='screen1-container'>
-      <h2>Insert a title for you recipe :</h2>
-      {/* REDUX STORE */}
-      {/* <h2>Title: {JSON.stringify(title)}</h2> */}
-      {/* ONCHANGE */}
-      {/* <h2>OnChange: {JSON.stringify(recipeTitle)}</h2> */}
+  function updateIngredient(index, value) {
+    dispatch(Actions.updateIngredient(index, value));
+  }
+
+  function addIngredient(value) {
+    dispatch(Actions.addIngredients(value));
+  }
+
+  function deleteIngredient(index) {
+    dispatch(Actions.deleteIngredients(index));
+  }
+
+  function saveIngredients() {
+    if (!ingredientTitle.length) {
+      return null;
+    } else {
+      addIngredient(ingredientTitle);
+    }
+  }
+
+  function renderIngredients() {
+    return ingredients.map((ingredient, index) => (
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          // setTitle();
         }}
       >
-        <div className='input-container'>
-          <Input
-            className={'title-input'}
-            onChange={(value) => {
-              setTitle(value);
-            }}
-            placeholder={'Example: Chocolate Cake'}
-            value={title}
-          />
-        </div>
+        <Input
+          className={'ingredient-input'}
+          setRecipeTitle={setIngredientTitle}
+          value={ingredient}
+          onChange={(value) => {
+            updateIngredient(index, value);
+          }}
+        />
+        ,
+        <Button
+          type='submit'
+          className='add-button'
+          Text={'Delete'}
+          onClick={() => {
+            deleteIngredient(index);
+          }}
+        ></Button>
+      </form>
+    ));
+  }
 
-        <Link to='/step2'>
+  if (!screen) {
+    return (
+      <div className='screen1-container'>
+        <h2>Insert a title for you recipe :</h2>
+        {/* REDUX STORE */}
+        {/* <h2>Title: {JSON.stringify(title)}</h2> */}
+        {/* ONCHANGE */}
+        {/* <h2>OnChange: {JSON.stringify(recipeTitle)}</h2> */}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            // setTitle();
+          }}
+        >
+          <div className='input-container'>
+            <Input
+              className={'title-input'}
+              onChange={(value) => {
+                setTitle(value);
+              }}
+              placeholder={'Example: Chocolate Cake'}
+              value={title}
+            />
+          </div>
+
           <Button
             type='submit'
             className={'save-button'}
             Text={'Save'}
             Disabled={!title.length}
+            onClick={() => {
+              changeScreen(!screen);
+            }}
           />
-        </Link>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  }
+
+  if (screen) {
+    return (
+      <div className='screen1-container'>
+        <div className='title-edit-container'>
+          <div className='instructions'>
+            <h2>Title: {JSON.stringify(title)} </h2>
+          </div>
+
+          <Button
+            className={'arrow-button'}
+            Text={'Edit'}
+            onClick={() => {
+              changeScreen(!screen);
+            }}
+          />
+        </div>
+
+        <div className='instructions'>
+          <h2>Add ingredients for you recipe :</h2>
+        </div>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            // setTitle();
+          }}
+        >
+          <div className='input-container'>
+            <Input
+              className={'ingredient-input'}
+              placeholder={'Example: Chocolate'}
+              value={ingredientTitle}
+              onChange={(value) => {
+                setIngredientTitle(value);
+              }}
+            />
+
+            <DropDownMenu />
+
+            <Button
+              type='submit'
+              className='add-button'
+              Text={'Add'}
+              Disabled={!ingredientTitle.length}
+              onClick={() => {
+                saveIngredients();
+                setIngredientTitle('');
+              }}
+            ></Button>
+          </div>
+        </form>
+
+        <div className='ingredients-list'>{renderIngredients()}</div>
+
+        {/* REDUX STORE */}
+        <h3>Ingredients: {JSON.stringify(ingredients)}</h3>
+        {/* ONCHANGE */}
+        <h3>onChange: {JSON.stringify(ingredientTitle)}</h3>
+      </div>
+    );
+  }
 }
 
 export default Screen1;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 import '../src/style.css';
 import Screen1 from './Views/Screen1';
-import Screen2 from './Views/Screen2';
 import Title from './Components/Title';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -15,7 +14,6 @@ ReactDOM.render(
         <Title path='/' exact component={Screen1} />
         <Switch>
           <Route path='/' exact component={Screen1} />
-          <Route path='/step2' exact component={Screen2} />
         </Switch>
       </Router>
     </Provider>
